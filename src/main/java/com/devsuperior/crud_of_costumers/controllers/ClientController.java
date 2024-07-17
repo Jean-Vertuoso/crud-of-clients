a068a50ee064b0dd4fd115eb60e4eec037acf170
6115820 <6115820@TR-BMLVT04.ead.thomsonreuters.com>
2024-07-17 18:00:08 -0300

    Implementacao inserção de novo recurso

--- src/main/java/com/devsuperior/crud_of_costumers/controllers/ClientController.java
+++ src/main/java/com/devsuperior/crud_of_costumers/controllers/ClientController.java
@@ -1,13 +1,20 @@
 package com.devsuperior.crud_of_costumers.controllers;
 
+import java.net.URI;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.crud_of_costumers.dto.ClientDTO;
 import com.devsuperior.crud_of_costumers.services.ClientService;
@@ -29,5 +36,13 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
-    }    
+    }
+    
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
 }
