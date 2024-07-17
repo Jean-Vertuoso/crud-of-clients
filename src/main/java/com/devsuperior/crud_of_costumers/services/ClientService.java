7101f60b65a450a97112770231a8b07927976cb5
6115820 <6115820@TR-BMLVT04.ead.thomsonreuters.com>
2024-07-17 13:33:25 -0300

    Implementacao busca por id

--- src/main/java/com/devsuperior/crud_of_costumers/services/ClientService.java
+++ src/main/java/com/devsuperior/crud_of_costumers/services/ClientService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.crud_of_costumers.dto.ClientDTO;
 import com.devsuperior.crud_of_costumers.entities.Client;
 import com.devsuperior.crud_of_costumers.repositories.ClientRepository;
+import com.devsuperior.crud_of_costumers.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,6 +21,13 @@ public class ClientService {
     public Page<ClientDTO> findAll(Pageable pageable){
         Page<Client> result = repository.findAll(pageable);
         return result.map(x -> new ClientDTO(x));
+    }
+    
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id){
+        Client client = repository.findById(id).orElseThrow(
+                        () -> new ResourceNotFoundException("ID não encontrado."));
+        return new ClientDTO(client);
     }
     
 }
