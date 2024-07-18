50a77443b3433743930c2e42656b5a33620f148e
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2024-07-17 21:59:07 -0300

    Implementacao atualizar recurso

--- src/main/java/com/devsuperior/crud_of_costumers/services/ClientService.java
+++ src/main/java/com/devsuperior/crud_of_costumers/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.crud_of_costumers.services;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -36,6 +38,18 @@ public class ClientService {
         copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ClientDTO(entity);
+    }
+    
+    @Transactional()
+    public ClientDTO update(Long id, ClientDTO dto){
+        try {
+            Client entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ClientDTO(entity);            
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("ID "+ id +" não foi encontrado.");
+        }
     }
 
     private void copyDtoToEntity(ClientDTO dto, Client entity) {
